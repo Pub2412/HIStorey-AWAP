49d5a649cf2c986eb30a750f675bb4d3cd7f1ddc
Lance Haboc <lance.haboc204@gmail.com>
2026-06-24 12:50:56 +0800

    program architecture

--- app.js
+++ app.js
@@ -3,23 +3,26 @@ const app = express();
 const cors = require('cors')
 const path = require('path');
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
-const items = require('./routes/item');
-const users = require('./routes/user');
-const orders = require('./routes/order');
-const dashboard = require('./routes/dashboard')
+// const items = require('./routes/item');
+// const users = require('./routes/user');
+// const orders = require('./routes/order');
+// const dashboard = require('./routes/dashboard')
 
-// app.get('/', (req, res) => {
-//     res.send('Hello from nodejs!')
-// })
-app.use(cors())
-app.use(express.json())
-app.use('/images', express.static(path.join(__dirname, 'images')))
-// app.use(express.urlencoded({ extended: true }));
+// // app.get('/', (req, res) => {
+// //     res.send('Hello from nodejs!')
+// // })
+// app.use(cors())
+// app.use(express.json())
+// app.use('/images', express.static(path.join(__dirname, 'images')))
+// // app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1', items);
-app.use('/api/v1', users);
-app.use('/api/v1', orders);
-app.use('/api/v1', dashboard);
+// app.use('/api/v1', items);
+// app.use('/api/v1', users);
+// app.use('/api/v1', orders);
+// app.use('/api/v1', dashboard);
 
 module.exports = app
